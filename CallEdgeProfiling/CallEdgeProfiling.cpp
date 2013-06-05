92b1f7f0fa951e7825831f737fc9163d8d8b7264
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2013-06-05 12:53:20 +0000

    Removed bug workaround.
    Removed LLVM bug 15253 workaround as it has since been fixed in r182387 (as has LLVM bug 15767).

--- CallEdgeProfiling/CallEdgeProfiling.cpp
+++ CallEdgeProfiling/CallEdgeProfiling.cpp
@@ -173,17 +173,6 @@ namespace soaap {
       }
 
       InsertProfilingInitCall(Main, "soaap_start_call_edge_profiling", Counters);
-
-      //WORKAROUND: remove calls to llvm.ptr.annotate.p0i8, otherwise LLVM will
-      //            crash when generating object code.
-      if (Function* F = M.getFunction("llvm.ptr.annotation.p0i8")) {
-        outs() << "BUG WORKAROUND: Removing calls to intrinisc @llvm.ptr.annotation.p0i8\n";
-        for (User::use_iterator u = F->use_begin(), e = F->use_end(); e!=u; u++) {
-          IntrinsicInst* intrinsicCall = dyn_cast<IntrinsicInst>(u.getUse().getUser());
-          BasicBlock::iterator ii(intrinsicCall);
-          ReplaceInstWithValue(intrinsicCall->getParent()->getInstList(), ii, intrinsicCall->getOperand(0));
-        }
-      }
       return true;
     }
 
