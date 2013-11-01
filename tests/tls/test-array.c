0fa9aaea75dfbf5fe47e6832197cac9062179ed2
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-11-01 19:35:19 +0000

    TLS tests: just ensure SOAAP doesn't segfault.

--- tests/tls/test-array.c
+++ tests/tls/test-array.c
@@ -1,6 +1,9 @@
 /*
  * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
- * RUN: soaap -o %t.soaap.ll %t.ll | FileCheck %s
+ * RUN: soaap -o %t.soaap.ll %t.ll > %t.out
+ * RUN: FileCheck %s -input-file %t.out
+ *
+ * This test simply checks that SOAAP runs correctly (e.g. doesn't segfault).
  *
  * CHECK: Running Soaap Pass
  */
--- tests/tls/test-printf.c
+++ tests/tls/test-printf.c
@@ -1,12 +1,13 @@
 /*
  * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
- * RUN: soaap -o %t.soaap.ll %t.ll | FileCheck %s
- *
- * CHECK: Running Soaap Pass
+ * RUN: soaap -o %t.soaap.ll %t.ll
+ * RUN: clang %t.soaap.ll -o %t
+ * RUN: %t | FileCheck %s
  */
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+	// CHECK: 1 -> 2
 	printf("%d -> %d\n", 1, 2);
 	return 0;
 }
