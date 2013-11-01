d2caf4a1602a2468987e58245ded3ed65ff3ea2f
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-11-01 11:21:05 +0000

    Updated a test; found a regression!
    Update the 'test-sandbox' unit test to use the new annotations. It seems that we have a regression: read() is being flagged as invalid even though the descriptor in question has been annotated with __soaap_fp_read.

--- tests/test-sandbox.c
+++ tests/test-sandbox.c
@@ -1,6 +1,7 @@
 /*
  * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
- * RUN: soaap -o %t.soaap.ll %t.ll | FileCheck %s
+ * RUN: soaap -o %t.soaap.ll %t.ll > %t.out
+ * RUN: FileCheck %s -input-file %t.out
  *
  * CHECK: Running Soaap Pass
  */
@@ -8,19 +9,23 @@
 #include <unistd.h>
 #include "soaap.h"
 
-int x __var_read;
+int x __soaap_var_read("sandbox named foo");
 void f();
 
 int main() {
+   // CHECK-DAG: *** Found call to sandbox entrypoint "f" that is not preceded by sandbox creation
   f(3,4);
   return 0;
 }
 
-__sandbox_persistent
-void f(int __fd_read ifd, int ofd) {
+__soaap_sandbox_persistent("sandbox named foo")
+void f(int __soaap_fd_read ifd, int ofd) {
   printf("hello from sandbox\n");
   char buf[10];
+  // CHECK-DAG: Sandboxed method "f" [sandbox named foo] wrote to global variable "x" ({{.*}}) but is not allowed to
   x = 3;
+  // CHECK-NOT: Insufficient privileges for "read()" in sandboxed method "f"
   read(ifd, buf, 1);
+  // CHECK-DAG: Insufficient privileges for "write()" in sandboxed method "f"
   write(ofd, buf, 1);
 }
