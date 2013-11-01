c7a7b794d78e0716acbd8dbcd25b7b4c569a221c
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-11-01 19:34:16 +0000

    Add expectations to 'provenance' tests.

--- tests/provenance/test-annotation1.c
+++ tests/provenance/test-annotation1.c
@@ -1,11 +1,20 @@
 /*
  * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
- * RUN: soaap -o %t.soaap.ll %t.ll | FileCheck %s
+ * RUN: soaap -o %t.soaap.ll %t.ll > %t.out
+ * RUN: soaap -soaap-vulnerable-vendors=org.gnu.libc -o /dev/null %t.ll > %t.vuln.out
  *
- * CHECK: Running Soaap Pass
+ * RUN: FileCheck %s -input-file %t.out -check-prefix=SAFE
+ * RUN: FileCheck %s -input-file %t.vuln.out -check-prefix=VULN
+ *
+ * SAFE: Running Soaap Pass
+ * VULN: Running Soaap Pass
  */
 #include "soaap.h"
 
+/*
+ * SAFE-NOT: org.gnu.libc is a vulnerable vendor
+ * VULN: org.gnu.libc is a vulnerable vendor
+ */
 __soaap_provenance("org.gnu.libc")
 
 int b();
