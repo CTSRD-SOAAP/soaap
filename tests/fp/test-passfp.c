6927f690d27b9bb73f7e32ae53bce442a9636910
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-11-04 14:28:41 +0000

    Flesh out fp/test-passfp.c.

--- tests/fp/test-passfp.c
+++ tests/fp/test-passfp.c
@@ -1,6 +1,8 @@
 /*
  * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
  * RUN: soaap -o %t.soaap.ll %t.ll | FileCheck %s
+ * RUN: soaap -o %t.soaap.ll %t.ll > %t.out
+ * RUN: FileCheck %s -input-file %t.out
  *
  * CHECK: Running Soaap Pass
  */
@@ -13,5 +15,6 @@ void f(int ifd) {
 int main(int argc, char** argv) {
   FILE* input = fopen(argv[1], "r");
   int ifd = fileno(input);
+  // CHECK-NOT: ***
   f(ifd);
 }
