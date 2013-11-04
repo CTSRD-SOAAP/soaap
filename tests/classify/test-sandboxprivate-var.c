1581a44c8db2b97a4f6c8191351c6b3b7716d559
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-11-04 13:41:41 +0000

    Flesh out test-sandboxprivate-var.c.

--- tests/classify/test-sandboxprivate-var.c
+++ tests/classify/test-sandboxprivate-var.c
@@ -1,13 +1,14 @@
 /*
  * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
- * RUN: soaap -o %t.soaap.ll %t.ll | FileCheck %s
+ * RUN: soaap -o %t.soaap.ll %t.ll > %t.out
+ * RUN: FileCheck %s -input-file %t.out
  *
  * CHECK: Running Soaap Pass
  */
 #include "soaap.h"
 #include <string.h>
 
-int sensitive __soaap_sandbox_private("network") = 25;
+int sensitive __soaap_private("network") = 25;
 
 void dostuff();
 void domorestuff();
@@ -19,14 +20,17 @@ int main() {
   return 0;
 }
 
-__soaap_sandbox_persistent_named("network")
+__soaap_sandbox_persistent("network")
 void dostuff() {
+
+  // CHECK-NOT: Sandboxed method "dostuff" [network] read global variable "sensitive" ({{.*}}) but is not allowed to
   int y = sensitive;
   printf("secret y is: %d\n", y);
 }
 
-__soaap_sandbox_persistent_named("box2")
+__soaap_sandbox_persistent("box2")
 void domorestuff() {
+  // CHECK: Sandboxed method "domorestuff" [box2] read global variable "sensitive" ({{.*}}) but is not allowed to
   int z = sensitive;
   printf("secret is: %d\n", z);
 }
