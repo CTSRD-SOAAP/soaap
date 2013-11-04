156f550a7583b37128bc097ae095dc0547e13d5e
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-11-04 13:28:40 +0000

    The clearance should permit this read.

---
+++ tests/classify/test-secret-var-allowed.c
@@ -0,0 +1,31 @@
+/*
+ * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
+ * RUN: soaap -o %t.soaap.ll %t.ll > %t.out
+ * RUN: FileCheck %s -input-file %t.out
+ *
+ * CHECK: Running Soaap Pass
+ */
+#include "soaap.h"
+#include <string.h>
+
+int sensitive __soaap_classify("secret");
+
+void dostuff();
+
+int main() {
+  sensitive = 25;
+  dostuff();
+  return 0;
+}
+
+__soaap_sandbox_persistent("foo")
+__soaap_clearance("secret")
+void dostuff() {
+  /*
+   * CHECK-NOT: *** Sandboxed method "dostuff" [foo]
+   * CHECK-NOT:     read global variable "sensitive"
+   * CHECK-NOT:     but is not allowed to.
+   */
+  int y = sensitive;
+  printf("secret y is: %d\n", y);
+}
