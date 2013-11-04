831cb3b098f81bbea654880c00024c76e0ba5023
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2013-11-04 17:31:31 +0000

    Propagate function assignments back to callers.
    If a function is assigned to a field of a struct, that is passed in as an argument
    we need to propagate this assignment back to the caller context.

--- SoaapPass/Analysis/InfoFlow/FPInferredTargetsAnalysis.cpp
+++ SoaapPass/Analysis/InfoFlow/FPInferredTargetsAnalysis.cpp
@@ -23,6 +23,7 @@ void FPInferredTargetsAnalysis::initialise(ValueContextPairList& worklist, Modul
 
   for (Module::iterator F = M.begin(), E = M.end(); F != E; ++F) {
     if (F->isDeclaration()) continue;
+    DEBUG(dbgs() << F->getName() << "\n");
     for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
       if (StoreInst* S = dyn_cast<StoreInst>(&*I)) { // assignments
         //S->dump();
@@ -60,10 +61,42 @@ void FPInferredTargetsAnalysis::initialise(ValueContextPairList& worklist, Modul
               }
               Lvar = Lvar->stripPointerCasts();
             }
-            //dbgs() << *Lvar << " = " << T->getName() << "()\n";
-            //DEBUG(dbgs() << INDENT_1 << "       Now adding " << *Lvar << " to worklist\n");
+            DEBUG(dbgs() << *Lvar << " = " << T->getName() << "()\n");
             state[ContextUtils::SINGLE_CONTEXT][Lvar] = targets;
             addToWorklist(Lvar, ContextUtils::SINGLE_CONTEXT, worklist);
+
+            // if Lvar is a struct parameter, then it probably outlives this function and
+            // so we should propagate the targets of function pointers it contains to the 
+            // calling context (i.e. to the corresponding caller's arg value)
+            if (AllocaInst* A = dyn_cast<AllocaInst>(Lvar)) {
+              string name = A->getName().str();
+              int suffixIdx = name.find(".addr");
+              if (suffixIdx != -1) {
+                // it's an alloca for a param, now we find which one
+                //dbgs() << "Name with suffix: " << name << "\n";
+                name = name.substr(0, suffixIdx);
+                //dbgs() << "Name without suffix: " << name << "\n";
+                // find if there is an arg with the same name
+                int i=0;
+                for (Function::arg_iterator I = T->arg_begin(), E = T->arg_end(); I != E; I++) {
+                  Argument* A2 = dyn_cast<Argument>(*&I);
+                  if (A2->getName() == name) {
+                    DEBUG(dbgs() << INDENT_1 << "Arg " << i << " has name " << A2->getName() << " (" << name << ")" << "\n");
+                    break;
+                  }
+                  i++;
+                }
+                if (i < T->arg_size()) {
+                  // we found the param index, propagate back to all caller args
+                  for (CallInst* caller : CallGraphUtils::getCallers(T, M)) {
+                    Value* arg = caller->getArgOperand(i);
+                    DEBUG(dbgs() << INDENT_2 << "Adding arg " << *arg << " to worklist\n");
+                    state[ContextUtils::SINGLE_CONTEXT][arg] = targets;
+                    addToWorklist(arg, ContextUtils::SINGLE_CONTEXT, worklist);
+                  }
+                }
+              }
+            }
           }
         }
       }
