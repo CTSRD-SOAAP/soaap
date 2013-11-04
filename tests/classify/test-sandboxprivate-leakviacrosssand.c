7740ebd69a46a45bc0a061c656887e69bf868004
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-11-04 14:13:08 +0000

    Fix test-sandboxprivate-leakviacrosssand.c.

--- tests/classify/test-sandboxprivate-leakviacrosssand.c
+++ tests/classify/test-sandboxprivate-leakviacrosssand.c
@@ -1,6 +1,7 @@
 /*
  * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
- * RUN: soaap -o %t.soaap.ll %t.ll | FileCheck %s
+ * RUN: soaap -o %t.soaap.ll %t.ll > %t.out
+ * RUN: FileCheck %s -input-file %t.out
  *
  * CHECK: Running Soaap Pass
  */
@@ -15,14 +16,17 @@ int main() {
   return 0;
 }
 
-__soaap_sandbox_persistent_named("get")
+__soaap_sandbox_persistent("get")
 void dostuff1() {
   int key = 813;
   printf("leaking sandbox-private password to another sandbox\n");
+
+  // CHECK: "dostuff1" executing in sandboxes: [get]
+  // CHECK: may leak private data through a cross-sandbox call into: [auth]
   dostuff2(key);
 }
 
-__soaap_sandbox_persistent_named("auth")
+__soaap_sandbox_persistent("auth")
 void dostuff2(int x) {
   printf("x: %d\n", x);
 }
