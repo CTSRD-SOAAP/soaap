fd019b3d1beda75e195b93261f080851da65f45a
Jonathan Anderson <jonathan.anderson@ieee.org>
2013-11-04 14:22:13 +0000

    Flesh out fp/test.c.

--- tests/fp/test.c
+++ tests/fp/test.c
@@ -1,12 +1,16 @@
 /*
  * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
- * RUN: soaap -o %t.soaap.ll %t.ll | FileCheck %s
+ * RUN: soaap -o %t.soaap.ll %t.ll > %t.soaap.out
+ * RUN: FileCheck %s -input-file %t.soaap.out
+ * RUN: clang %t.soaap.ll -o %t
+ * RUN: %t | FileCheck %s -check-prefix=RUNTIME
  *
  * CHECK: Running Soaap Pass
  */
 #include <stdio.h>
 
 void a() {
+	// RUNTIME: Hello from a
 	printf("Hello from a\n");
 }
 
