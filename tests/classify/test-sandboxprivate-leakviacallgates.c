de277d1d2ea44b5f7994f81bb6e4d7f58909f99f
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2013-11-08 09:51:07 +0000

    Fixed annotations and added CHECK

--- tests/classify/test-sandboxprivate-leakviacallgates.c
+++ tests/classify/test-sandboxprivate-leakviacallgates.c
@@ -10,16 +10,20 @@
 void dostuff();
 void privfunc(int p1);
 
-__soaap_callgates(privfunc);
+__soaap_callgates(box, privfunc);
 
 int main() {
   dostuff();
   return 0;
 }
 
-__soaap_sandbox_persistent_named("box")
+__soaap_sandbox_persistent("box")
 void dostuff() {
-  int key __soaap_sandbox_private("box");
+  int key __soaap_private("box");
+  /*
+   * CHECK: *** Sandboxed method "dostuff" executing in sandboxes: [box]
+   * CHECK:     may leak private data through callgate privfunc
+   */
   privfunc(key);
 }
 
