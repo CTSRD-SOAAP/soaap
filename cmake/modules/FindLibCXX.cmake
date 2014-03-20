913f40b041b9500288f2526a64b1a0fc6daed1d1
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2014-03-20 14:57:31 +0000

    Use libc++ if available

---
+++ cmake/modules/FindLibCXX.cmake
@@ -0,0 +1,33 @@
+# - Find libc++
+# Find libc++ if installed
+#
+# This module defines the following variables:
+#     LIBCXX_FOUND         - whether or not we found libc++
+#     LIBCXX_INCLUDE_DIR   - the include directory where C++ headers live
+#     LIBCXX_INCLUDE_DIRS  - include dirs for libc++ and (no) dependencies
+#
+
+find_library(LIBCXX_LIBRARY c++)
+set(LIBCXX_LIBRARIES "${LIBCXX_LIBRARY}")
+
+# On Mac OS X, the headers are in a slightly unusual place (e.g. /usr/lib/c++).
+
+find_path(LIBCXX_PREFIX c++/v1/string REQUIRED
+  DOC "container for versioned libc++ include directories"
+  PATHS ${CMAKE_SYSTEM_PREFIX_PATH}
+)
+
+if (LIBCXX_PREFIX)
+  set(LIBCXX_INCLUDE_DIR "${LIBCXX_PREFIX}/c++/v1"
+    CACHE STRING "Include directory for libc++ headers")
+endif ()
+
+set(LIBCXX_INCLUDE_DIRS "${LIBCXX_INCLUDE_DIR}")
+
+include (FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LIBCXX DEFAULT_MSG
+  LIBCXX_LIBRARY
+  LIBCXX_INCLUDE_DIR
+)
+
+mark_as_advanced (LIBCXX_INCLUDE_DIR)
