ce2575971a1be8110ad0fcc82fddc4ddcc71c70e
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2014-04-10 16:35:27 +0000

    Fix expected output in test 'globals-lost-write.c'

--- tests/globals/globals-lost-write.c
+++ tests/globals/globals-lost-write.c
@@ -14,7 +14,7 @@ void f();
 
 int main() {
   __soaap_create_persistent_sandbox("sandbox named foo");
-  // CHECK-DAG: * Write to shared variable "x" outside sandbox in method "main" will not be seen by the sandboxes: [sandbox named foo]
+  // CHECK-DAG: * Write to shared variable "x" ({{.*}}) outside sandbox in method "main" will not be seen by the sandboxes: [sandbox named foo]
   x = 4;
   f();
   return 0;
