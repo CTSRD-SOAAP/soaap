3a3d580f8ce85a3c59ebcb23f2e22809561b4063
Jonathan Anderson <jonathan.anderson@ieee.org>
2014-08-06 17:04:21 +0100

    Add support for non-installed libc++.
    Sometimes (e.g., on continuous integration machines) we want to build with a
    libc++ that hasn't been installed to /usr/lib/c++/v1. With this commit, we can
    specify a LIBCXX_BUILD_DIR and LIBCXX_SOURCE_DIR to pick up headers and the
    library itself from a source and build tree.

--- cmake/modules/FindLibCXX.cmake
+++ cmake/modules/FindLibCXX.cmake
@@ -3,12 +3,8 @@
 #
 # This module defines the following variables:
 #     LIBCXX_FOUND         - whether or not we found libc++
-#     LIBCXX_INCLUDE_DIR   - the include directory where C++ headers live
 #     LIBCXX_INCLUDE_DIRS  - include dirs for libc++ and (no) dependencies
 #
-
-find_library(LIBCXX_LIBRARY c++)
-set(LIBCXX_LIBRARIES "${LIBCXX_LIBRARY}")
 
 find_path(LIBCXX_PREFIX c++/v1/string REQUIRED
   DOC "container for versioned libc++ include directories"
@@ -18,14 +14,28 @@ find_path(LIBCXX_PREFIX c++/v1/string REQUIRED
 if (LIBCXX_PREFIX)
   set(LIBCXX_INCLUDE_DIR "${LIBCXX_PREFIX}/c++/v1"
     CACHE STRING "Include directory for libc++ headers")
+
+  set(LIBCXX_INCLUDE_DIRS "${LIBCXX_INCLUDE_DIR}" "${LIBCXX_PREFIX}")
+
+  find_library(LIBCXX_LIBRARY c++)
+
+elseif (LIBCXX_BUILD_DIR)
+  set(LIBCXX_INCLUDE_DIRS
+    "${LIBCXX_BUILD_DIR}/include"
+    "${LIBCXX_SOURCE_DIR}/include")
+
+  find_library(LIBCXX_LIBRARY c++
+	PATHS "${LIBCXX_BUILD_DIR}/lib"
+	NO_DEFAULT_PATH)
+
 endif ()
 
-set(LIBCXX_INCLUDE_DIRS "${LIBCXX_INCLUDE_DIR}" "${LIBCXX_PREFIX}")
+set(LIBCXX_LIBRARIES "${LIBCXX_LIBRARY}")
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBCXX DEFAULT_MSG
   LIBCXX_LIBRARY
-  LIBCXX_INCLUDE_DIR
+  LIBCXX_INCLUDE_DIRS
 )
 
-mark_as_advanced (LIBCXX_INCLUDE_DIR)
+mark_as_advanced (LIBCXX_INCLUDE_DIRS)
