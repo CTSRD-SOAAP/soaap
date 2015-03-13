be24a2a5cd9858c51bc724c44e843c7e2a288d2a
Khilan Gudka <Khilan.Gudka@cl.cam.ac.uk>
2015-03-13 12:14:42 +0000

    Add XFAIL to seccomp test

--- tests/syscalls/seccomp.c
+++ tests/syscalls/seccomp.c
@@ -2,6 +2,7 @@
  * RUN: clang %cflags -emit-llvm -S %s -o %t.ll
  * RUN: soaap --soaap-infer-fp-targets --soaap-sandbox-platform=seccomp -o %t.soaap.ll %t.ll > %t.out
  * RUN: FileCheck %s -input-file %t.out
+ * XFAIL: *
  *
  * CHECK: Running Soaap Pass
  */
@@ -21,15 +22,16 @@ int main(int argc, char** argv) {
 
 __soaap_sandbox_persistent("sandbox")
 void foo() {
+  fp = bar;
   // CHECK: *** Sandbox "sandbox" performs system call "open" but it is not allowed to,
   // CHECK-NEXT: *** based on the current sandboxing restrictions.
-  // CHECK-NEXT: +++ Line 31 of file {{.*}}
+  // CHECK-NEXT: +++ Line 32 of file {{.*}}
   fp();
 }
 
 void bar() {
   open("somefile", O_CREAT);
   // CHECK: Sandbox "sandbox" performs system call "read" but is not allowed to for the given fd arg
-  // CHECK-NEXT: +++ Line 34 of file {{.*}}
+  // CHECK-NEXT: +++ Line 35 of file {{.*}}
   read(2, NULL, 0);
 }
