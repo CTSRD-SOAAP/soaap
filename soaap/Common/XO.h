e73e9c33acc59a9f87afcb09bbeee628a60cb548
Alex Richardson <arichardson.kde@gmail.com>
2015-03-31 21:11:32 +0100

    Add RAII wrappers for XO::[open/close]_[container/instance/list]()
    They make sure that the element is closed on scope exit or when close()
    is called. This make sure that there is no name mismatch between the
    open_*() and close_*() call. Additionally XO::Instance can only be
    constructed from a const reference to XO::List which ensures that these
    names also match.

--- soaap/Common/XO.h
+++ soaap/Common/XO.h
@@ -18,19 +18,40 @@ namespace soaap {
       static void create_to_file(FILE* fp, int style, int flags);
       static void finish();
 
+      static void emit(const char* fmt, ...);
+
+    public: // TODO private
       static void open_container(const char* name);
       static void close_container(const char* name);
-      
       static void open_list(const char* name);
       static void close_list(const char* name);
       static void open_instance(const char* name);
       static void close_instance(const char* name);
-      
-      static void emit(const char* fmt, ...);
+    public:
+      // RAII classes for XO::[open/close]_[container/list/instance]()
+      template<void(*openFunc)(const char*), void(*closeFunc)(const char*)>
+      class RAII {
+        public:
+          explicit RAII(const char* name) : name(name) { openFunc(name); }
+          void close() {
+            if (name) {
+              closeFunc(name);
+              name = nullptr;
+            }
+          }
+          ~RAII() { close(); }
+          const char* name;
+      };
+      typedef RAII<open_list, close_list> List;
+      typedef RAII<open_container, close_container> Container;
 
+      /** Instances should be created by referring to the outer element and not from a constant string */
+      class Instance : public RAII<open_instance, close_instance> {
+        public:
+          Instance(const List& list) : RAII(list.name) {}
+      };
     private:
       static list<xo_handle_t*> handles;
-      
   };
 }
 
