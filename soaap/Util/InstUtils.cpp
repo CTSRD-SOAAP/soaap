3191c78442919850f85a4266c4cfa2930f65f1ba
Alex Richardson <arichardson.kde@gmail.com>
2015-03-31 22:44:24 +0100

    port Util/ to the new libxo RAII classes

--- soaap/Util/InstUtils.cpp
+++ soaap/Util/InstUtils.cpp
@@ -9,7 +9,7 @@ using namespace soaap;
 
 void InstUtils::emitInstLocation(Instruction* I) {
   if (MDLocation* loc = dyn_cast_or_null<MDLocation>(I->getMetadata("dbg"))) {
-    XO::open_container("location");
+    XO::Container locationContainer("location");
     XO::emit(
       " +++ Line {:line/%d} of file {:file/%s}",
       loc->getLine(),
@@ -19,6 +19,5 @@ void InstUtils::emitInstLocation(Instruction* I) {
       XO::emit(" ({:library/%s} library)", library.c_str());
     }
     XO::emit("\n");
-    XO::close_container("location");
   }
 }
