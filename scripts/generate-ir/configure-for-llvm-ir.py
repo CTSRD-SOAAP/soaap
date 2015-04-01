8890ce89ec047bde660b9116dc56faa571c69fb9
Alex Richardson <arichardson.kde@gmail.com>
2015-04-01 17:00:09 +0100

    Allow passing parameters in the configure overrides
    This simplifies the wrapper invocation for qtbase because for some
    reason they decided to have AR be the whole command with paramters and
    not just the path to the variable
    
    configure-for-llvm-ir.py '--ar=ar cqs'

--- scripts/generate-ir/configure-for-llvm-ir.py
+++ scripts/generate-ir/configure-for-llvm-ir.py
@@ -12,25 +12,30 @@ changedEnvVars = {}
 def changeEnvVar(var, value):
     os.environ[var] = value
     changedEnvVars[var] = value
-
+    print('set', var, 'to', value)
 
 def setIrWrapperEnvVar(var, command):
+    splitted = command.split()
+    command = splitted[0]
     wrapper = os.path.join(scriptDir, command + '-and-emit-llvm-ir.py')
     if not os.path.exists(wrapper):
         sys.exit('could not find ' + wrapper)
+    # allow parameters to be passed
+    if len(splitted) > 1:
+        wrapper = wrapper + ' ' + ' '.join(splitted[1:])
     changeEnvVar(var, wrapper)
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('options', nargs='*', help='Arguments to pass to configure')
 parser.add_argument('-f', required=False, default='./configure', help='configure script override')
 parser.add_argument('--ld', required=False, default='clang', type=str,
-                    help='LD wrapper script name')
+                    help='LD wrapper script name (and parameters)')
 parser.add_argument('--link', required=False, default='clang', type=str,
-                    help='LINK wrapper script name')
+                    help='LINK wrapper script name (and parameters)')
 parser.add_argument('--ar', required=False, default='ar', type=str,
-                    help='AR wrapper script name')
+                    help='AR wrapper script name (and parameters)')
 parser.add_argument('--ranlib', required=False, default='ar', type=str,
-                    help='RANLIB wrapper script name')
+                    help='RANLIB wrapper script name (and parameters)')
 parser.add_argument('--env', required=False, action='append', metavar=('VAR=VALUE'),
                     help='override env var [VAR] with [VALUE]. Can be repeated multiple times')
 parser.add_argument('--confirm', action='store_true', help='Confirm before running configure')
