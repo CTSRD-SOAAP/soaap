cdd68d00d0fe1f5d4b21d0c9c432bea136acd6c6
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-04-27 15:10:15 -0230

    Commit *most recent* ExternalLibXO.cmake.

--- cmake/modules/ExternalLibXO.cmake
+++ cmake/modules/ExternalLibXO.cmake
@@ -1,23 +1,36 @@
-if (CMAKE_SYSTEM_NAME MATCHES "*BSD*")
+# Build libxo as an external project.
+#
+# This module defines the following variables:
+#     LIBXO_INCLUDE_DIRS  - include dirs for libxo
+#     LIBXO_LIBRARIES     - libs needed to use libxo
+#     LIBXO_PREFIX        - prefix (e.g., /usr/local) of all libxo files
+#     LIBXO_VERSION       - version of libxo fetched from GitHub
+
+if (CMAKE_SYSTEM_NAME MATCHES ".*BSD")
   set(GMAKE "gmake")
 else()
   set(GMAKE "make")
 endif()
 
-message(STATUS "Building external libxo ${SYSTEM_XO_VERSION}")
+set(LIBXO_VERSION ${LIBXO_NEED_VERSION})
+
+message(STATUS "Building external libxo ${LIBXO_VERSION}")
 include(ExternalProject)
 ExternalProject_Add(libxo
   GIT_REPOSITORY https://github.com/Juniper/libxo.git
-  GIT_TAG 0.1.6
+  GIT_TAG ${LIBXO_VERSION}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND rm -rf configure m4
                     COMMAND sh bin/setup.sh
                     COMMAND cd build
-                    COMMAND ../configure --prefix=<INSTALL_DIR>
+                    COMMAND ../configure --prefix=<INSTALL_DIR> --libdir=<INSTALL_DIR>/lib
   BUILD_COMMAND cd <SOURCE_DIR>/build COMMAND ${GMAKE}
   INSTALL_COMMAND cd <SOURCE_DIR>/build COMMAND ${GMAKE} install
   STEP_TARGETS build install
 )
 
 ExternalProject_Get_Property(libxo install_dir)
-set(XO_PREFIX ${install_dir})
+set(LIBXO_PREFIX ${install_dir})
+
+set(LIBXO_INCLUDE_DIRS "${LIBXO_PREFIX}/include")
+set(LIBXO_LIBRARIES "${LIBXO_PREFIX}/lib/libxo.so")
